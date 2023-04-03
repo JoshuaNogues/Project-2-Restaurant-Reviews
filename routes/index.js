e728c62bf3fbd7c0164e920900f78a3d6be9b0a8
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-04-03 09:36:49 -0400

    fixed log in and log out buttons

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'revate' });
+
+router.get('/', (req, res) => {
+  res.render('index', { title: 'revate', session: req.session });
 });
+
 
 module.exports = router;
